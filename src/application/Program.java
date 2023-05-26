64141c38bdab18b76b712cd3288cd7b1325e1a3b
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2023-05-25 23:20:29 -0300

    firts class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("chess system");
+		Position pos= new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
