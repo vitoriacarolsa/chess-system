7e9edb699b6ff08fd91852d41278613d61103b24
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2023-05-27 00:40:55 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,34 +1,45 @@
 package application;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-
 public class Program {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner (System.in);
 		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		
 		while (true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.println("Source: ");
-			ChessPosition sorce=UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.println("Target:");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(sorce, target);
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
-		
-		
-		
-
 	}
-
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,30 +25,34 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
-		Position source= sourcePosition.toPosition();
-		Position target=targetPosition.toPosition();
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
-		Piece capturedPiece =makeMove(source, target);
-		return (ChessPiece) capturedPiece;
+		Piece capturedPiece = makeMove(source, target);
+		return (ChessPiece)capturedPiece;
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p=board.removePiece(source);
-		Piece capturedPiece= board.removePiece(target);
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		return capturedPiece;
 	}
 	
-	
-	private void validateSourcePosition (Position position) {
+	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPice(position)) {
-			throw new ChessException ("There is no piece on source position");
+			throw new ChessException("There is no piece on source position");
 		}
-		
 		if (!board.piece(position).isThereAnyPossibleMove()) {
-			throw new ChessException("There is no possible moves for the chosen ");
+			throw new ChessException("There is no possible moves for the chosen piece");
 		}
 	}
 	
@@ -57,10 +61,6 @@ public class ChessMatch {
 			throw new ChessException("The chosen piece can't move to target position");
 		}
 	}
-	
-	
-	
-	
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
